32c990b14ee99839eb33854577c857ced8552d66
Baran <gfbarann@gmail.com>
2023-04-05 06:14:07 +0300

    test

--- mybat.bat
+++ mybat.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Compiling Java source code...
-javac -cp C:\\Users\\Baran\\IdeaProjects\\API_Test\\src\\test\\java\\allRequest\\UserService.java
+javac -cp C:\Users\Baran\IdeaProjects\API_Test\src\test\java\allRequest\UserService.java
 echo Java source code compiled successfully.
